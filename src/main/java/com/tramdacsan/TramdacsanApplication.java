635926b7cceb5635b8848778aebeb348d9b30667
vanh251 <148326521+vanh251@users.noreply.github.com>
2026-03-05 00:58:02 +0700

    first commit

---
+++ src/main/java/com/tramdacsan/TramdacsanApplication.java
@@ -0,0 +1,13 @@
+package com.tramdacsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramdacsanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TramdacsanApplication.class, args);
+    }
+
+}
